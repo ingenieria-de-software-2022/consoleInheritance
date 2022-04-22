89e867024617497ca303b7fc9894c1958ae723d5
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-04-22 11:26:16 -0500

    feat: adding classes inheritance example

---
+++ ConsoleClasses/Ci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClasses
+{
+    internal class Ci
+    {
+        private double pi;
+        
+        public double Pi { get { return pi; } set { pi = value; } }
+        
+        public void Meti()
+        {
+            pi = 100.8;
+        }
+    }
+}
---
+++ ConsoleClasses/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ci oi = new Ci();
+
+            oi.Pi = 10.5;
+            oi.Meti();
+            Console.WriteLine(oi.Pi);
+
+            System.Diagnostics.Process.Start("WFcruzlara_signature.exe");
+
+            // Makes the program wait for a key press and it prevents the screen until a key is pressed.
+            Console.ReadKey();
+        }
+    }
+}
